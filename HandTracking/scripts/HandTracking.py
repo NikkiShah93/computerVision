0f3f261286867a121e6d09f341c6b96022b379f4
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-05-20 12:02:53 -0400

    added the fps

--- HandTracking/scripts/HandTracking.py
+++ HandTracking/scripts/HandTracking.py
@@ -1,7 +1,11 @@
 import numpy as np
 import cv2
 import mediapipe as mp
+import time
 
+## tracking the time
+past_time = 0
+curr_time = 0
 
 ## capturing the webcam 
 video = cv2.VideoCapture(0)
@@ -21,10 +25,16 @@ while True:
     if results.multi_hand_landmarks:
         for hand in results.multi_hand_landmarks:
             drawhands.draw_landmarks(img, hand, mphand.HAND_CONNECTIONS)
+    ## calculating the fps
+    curr_time = time.time()
+    fps = 1 / (curr_time - past_time)
+    past_time = curr_time
+    ## and the adding it to the image
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_COMPLEX_SMALL,3,
+                color=(0, 0, 0), thickness=1)
     
     ## showing the captured video
     cv2.imshow("Image", img)
-    
     cv2.waitKey(1)
 
 # When everything done, release the capture
